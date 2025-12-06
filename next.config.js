ba6bcedd66d5b29602da996b941b5f6daea32a69
ahmadlatifdev <privategate777@gmail.com>
2025-12-06 11:27:04 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
-    domains: ['cdn.cjdropshipping.com', 'img.alicdn.com', 'yourstore.com'],
+    domains: [
+      'cdn.cjdropshipping.com',
+      'img.alicdn.com',
+      'yourstore.com'
+    ],
   },
-  experimental: {
-    serverActions: true,
-  },
+
+  // REMOVE the old experimental.serverActions boolean (it breaks Next.js 14)
+  experimental: {},
 };
 
 module.exports = nextConfig;
+
